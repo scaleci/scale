b4a05609eae9cf2fe8830f83f647f5e9be8a440f
arunthampi <arun.thampi@gmail.com>
2017-12-25 22:20:02 -0800

    Return full paths

--- tests/glob.go
+++ tests/glob.go
@@ -1,12 +1,28 @@
 package tests
 
-import zglob "github.com/mattn/go-zglob"
+import (
+	"os"
+	"path/filepath"
+
+	zglob "github.com/mattn/go-zglob"
+)
 
 func Glob(pattern string) []string {
 	matches, err := zglob.Glob(pattern)
 	if err != nil {
 		return []string{}
 	} else {
-		return matches
+		fullPaths := []string{}
+
+		for _, m := range matches {
+			if fi, err := os.Stat(m); err == nil && fi.Mode().IsRegular() {
+				fullpath, err := filepath.Abs(m)
+				if err == nil {
+					fullPaths = append(fullPaths, fullpath)
+				}
+			}
+		}
+
+		return fullPaths
 	}
 }
