a8960e0cbd6fd9e6b77a0a592e354ad80d9d00ed
Arun Thampi <arun.thampi@gmail.com>
2017-12-26 21:13:48 -0800

    Revert "Return full paths"

--- tests/glob.go
+++ tests/glob.go
@@ -1,28 +1,12 @@
 package tests
 
-import (
-	"os"
-	"path/filepath"
-
-	zglob "github.com/mattn/go-zglob"
-)
+import zglob "github.com/mattn/go-zglob"
 
 func Glob(pattern string) []string {
 	matches, err := zglob.Glob(pattern)
 	if err != nil {
 		return []string{}
 	} else {
-		fullPaths := []string{}
-
-		for _, m := range matches {
-			if fi, err := os.Stat(m); err == nil && fi.Mode().IsRegular() {
-				fullpath, err := filepath.Abs(m)
-				if err == nil {
-					fullPaths = append(fullPaths, fullpath)
-				}
-			}
-		}
-
-		return fullPaths
+		return matches
 	}
 }
