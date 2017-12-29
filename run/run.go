16704ef5aa59543f36c9ff1f50ff9d7b4d19491c
arunthampi <arun.thampi@gmail.com>
2017-12-28 22:45:15 -0800

    Add build step

---
+++ run/run.go
@@ -0,0 +1,41 @@
+package run
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+)
+
+func Build(app *App) error {
+	cmdName := "docker"
+	cmdArgs := []string{
+		"build",
+		".",
+		"-f", app.GlobalConfig.BuildWith,
+		"-t", app.ImageName()}
+
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf("[docker build] %s\n", scanner.Text())
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
