9759639b89dfe8124d3e8fd67fbb9558bd6fcd1d
arunthampi <arun.thampi@gmail.com>
2017-12-28 22:59:12 -0800

    Start services (and stop cleanly on exit)

--- run/run.go
+++ run/run.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func Build(app *App) error {
@@ -35,6 +36,63 @@ func Build(app *App) error {
 	err = cmd.Wait()
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func StartServices(app *App) error {
+	for _, s := range app.Services {
+		err := startService(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func StopServices(app *App) error {
+	for _, s := range app.Services {
+		err := stopService(s)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func startService(s *Service) error {
+	cmdName := "docker"
+	cmdArgs := []string{
+		"run",
+		"-d",
+	}
+
+	cmdArgs = append(cmdArgs, s.PortsAsArgs()...)
+	cmdArgs = append(cmdArgs, s.Image)
+	cmd := exec.Command(cmdName, cmdArgs...)
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	s.ContainerID = strings.Trim(string(out), "\n")
+
+	fmt.Printf("[%s] Started with container ID %s\n", s.ID, s.ContainerID)
+	return nil
+}
+
+func stopService(s *Service) error {
+	if s.ContainerID != "" {
+		cmd := exec.Command("docker", "stop", s.ContainerID, "-t", 5)
+		_, err := cmd.Output()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("[%s] Stopped with container ID %s\n", s.ID, s.ContainerID)
+		return nil
 	}
 
 	return nil
--- run/service.go
+++ run/service.go
@@ -1,7 +1,18 @@
 package run
 
 type Service struct {
-	ID    string
-	Image string
-	Ports []string
+	ID          string
+	Image       string
+	Ports       []string
+	ContainerID string
+}
+
+func (s *Service) PortsAsArgs() []string {
+	portsAsArgs := []string{}
+
+	for _, p := range s.Ports {
+		portsAsArgs = append(portsAsArgs, []string{"-p", p}...)
+	}
+
+	return portsAsArgs
 }
