73ec4b7bd24c0bb834631e69b9b04bac390694e0
arunthampi <arun.thampi@gmail.com>
2018-01-05 07:19:07 -0800

    Minor refactor

--- tests/parallelize.go
+++ tests/parallelize.go
@@ -9,36 +9,42 @@ import (
 
 func Parallelize(protocol string, parallelCount int, opts map[string]string) error {
 	if protocol == "postgres" {
-		var userName string
-		var host string
-		var port string
-		var database string
-		var ok bool
+		return parallelizePostgres(parallelCount, opts)
+	}
 
-		if userName, ok = opts["user"]; !ok {
-			userName = os.Getenv("POSTGRES_USER")
-		}
-		if host, ok = opts["host"]; !ok {
-			host = os.Getenv("POSTGRES_HOST")
-		}
-		if port, ok = opts["port"]; !ok {
-			port = os.Getenv("POSTGRES_PORT")
-		}
-		if database, ok = opts["database"]; !ok {
-			database = os.Getenv("POSTGRES_DATABASE")
-		}
+	return nil
+}
 
-		for i := 0; i < parallelCount; i++ {
-			err := exec.Run("psql", []string{
-				"-U", userName,
-				"-h", host,
-				"-p", port,
-				"-c", fmt.Sprintf("CREATE DATABASE scale%d template '%s'", i, database),
-			}, "")
+func parallelizePostgres(parallelCount int, opts map[string]string) error {
+	var userName string
+	var host string
+	var port string
+	var database string
+	var ok bool
 
-			if err != nil {
-				return err
-			}
+	if userName, ok = opts["user"]; !ok {
+		userName = os.Getenv("POSTGRES_USER")
+	}
+	if host, ok = opts["host"]; !ok {
+		host = os.Getenv("POSTGRES_HOST")
+	}
+	if port, ok = opts["port"]; !ok {
+		port = os.Getenv("POSTGRES_PORT")
+	}
+	if database, ok = opts["database"]; !ok {
+		database = os.Getenv("POSTGRES_DATABASE")
+	}
+
+	for i := 0; i < parallelCount; i++ {
+		err := exec.Run("psql", []string{
+			"-U", userName,
+			"-h", host,
+			"-p", port,
+			"-c", fmt.Sprintf("CREATE DATABASE scale%d template '%s'", i, database),
+		}, "")
+
+		if err != nil {
+			return err
 		}
 	}
 
