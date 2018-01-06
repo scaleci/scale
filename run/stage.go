4e89436eade8d8483beaa11cfbcfaf80ac662839
arunthampi <arun.thampi@gmail.com>
2018-01-05 17:06:12 -0800

    Loop through stages depending on Parallelism

--- run/stage.go
+++ run/stage.go
@@ -17,30 +17,30 @@ type Stage struct {
 
 func (s *Stage) Run(totalContainers int64) error {
 	cmdName := "docker"
-	cmdArgs := []string{
-		"run",
-		"--mount",
-		fmt.Sprintf("type=bind,source=%s,target=/bin/scale", scaleBinaryPath),
+
+	for index := 0; index < s.Parallelism; index++ {
+		cmdArgs := []string{
+			"run",
+			"--mount",
+			fmt.Sprintf("type=bind,source=%s,target=/bin/scale", scaleBinaryPath),
+		}
+		env := s.ParentApp.Env()
+		// Inject "running state" into the container
+		// MAX is the total number of containers (across all stages) that are part of this run
+		env["SCALE_CI_MAX"] = fmt.Sprintf("%d", totalContainers)
+		// TOTAL is the parallelism of the current stage
+		env["SCALE_CI_TOTAL"] = fmt.Sprintf("%d", s.Parallelism)
+		// INDEX is the index of the current running container
+		env["SCALE_CI_INDEX"] = fmt.Sprintf("%d", index)
+
+		for k, v := range env {
+			cmdArgs = append(cmdArgs, "-e", fmt.Sprintf("%s=%s", k, v))
+		}
+
+		cmdArgs = append(cmdArgs, s.ParentApp.ImageName())
+		cmdArgs = append(cmdArgs, "/bin/bash", "-c")
+		cmdArgs = append(cmdArgs, s.Command)
+
+		return exec.Run(cmdName, cmdArgs, fmt.Sprintf("docker.run.%s", s.ID))
 	}
-
-	index := 0
-
-	env := s.ParentApp.Env()
-	// Inject "running state" into the container
-	// MAX is the total number of containers (across all stages) that are part of this run
-	env["SCALE_CI_MAX"] = fmt.Sprintf("%d", totalContainers)
-	// TOTAL is the parallelism of the current stage
-	env["SCALE_CI_TOTAL"] = fmt.Sprintf("%d", s.Parallelism)
-	// INDEX is the index of the current running container
-	env["SCALE_CI_INDEX"] = fmt.Sprintf("%d", index)
-
-	for k, v := range env {
-		cmdArgs = append(cmdArgs, "-e", fmt.Sprintf("%s=%s", k, v))
-	}
-
-	cmdArgs = append(cmdArgs, s.ParentApp.ImageName())
-	cmdArgs = append(cmdArgs, "/bin/bash", "-c")
-	cmdArgs = append(cmdArgs, s.Command)
-
-	return exec.Run(cmdName, cmdArgs, fmt.Sprintf("docker.run.%s", s.ID))
 }
