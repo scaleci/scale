63db72dc99a8422df0c12412921ce12804075dbe
Arun Thampi <arun.thampi@gmail.com>
2018-02-08 16:59:59 -0800

    Use the docker CLI to build the image
    since it's much faster, respects .dockerignore and everything. Closes #30

--- run/build.go
+++ run/build.go
@@ -1,53 +1,22 @@
 package run
 
 import (
-	"bytes"
-	"context"
-	"path/filepath"
+	"fmt"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
-	"github.com/mholt/archiver"
+	"github.com/scaleci/scale/exec"
 )
 
 func Build(app *App) error {
-	buildArgs := map[string]*string{}
-
-	dockerContext := new(bytes.Buffer)
-	dockerFile := app.GlobalConfig.BuildWith
-	path := filepath.Dir(dockerFile)
-
-	err := archiver.Tar.Write(dockerContext, []string{path})
-	if err != nil {
-		return err
-	}
-
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		return err
-	}
+	cmdName := "docker"
+	cmdArgs := []string{
+		"build",
+		".",
+		"-f", app.GlobalConfig.BuildWith,
+		"-t", app.ImageName()}
 
 	for k, v := range app.GlobalConfig.Env {
-		argValue := v
-		buildArgs[k] = &argValue
+		cmdArgs = append(cmdArgs, "--build-arg", fmt.Sprintf("%s=%s", k, v))
 	}
 
-	imageBuildResponse, err := cli.ImageBuild(
-		context.Background(),
-		dockerContext,
-		types.ImageBuildOptions{
-			Tags:       []string{app.ImageName()},
-			Context:    dockerContext,
-			Dockerfile: dockerFile,
-			BuildArgs:  buildArgs})
-
-	if err != nil {
-		return err
-	}
-
-	if err := StreamDockerResponse(imageBuildResponse.Body, "stream", "error"); err != nil {
-		return err
-	}
-
-	return nil
+	return exec.Run(cmdName, cmdArgs, "docker.build")
 }
